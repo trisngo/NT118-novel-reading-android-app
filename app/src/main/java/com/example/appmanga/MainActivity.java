9ecc09cdf31a06a0d2b395ab2af9c936dd222ad2
mamadevme <59251361+mamadevme@users.noreply.github.com>
2022-12-21 16:43:11 +0700

    fix color

--- app/src/main/java/com/example/appmanga/MainActivity.java
+++ app/src/main/java/com/example/appmanga/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -37,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         //init_database();
+
         replaceFragment(new HomeFragment(),key);
         key="home";
         //Sự kiện bấm thay đổi fragmanent
